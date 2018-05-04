a551b889f748cde88efc8a7c6a4d37bb32786fa3
voltae <ic17b096@technikum-wien.at>
2018-05-04 17:56:10 +0200

    Added Doxy comment for the .h file
    Signed-off-by: voltae <ic17b096@technikum-wien.at>

--- mypopen.h
+++ mypopen.h
@@ -23,8 +23,19 @@
 #include <assert.h> // for assert
 #include <sys/stat.h>
 
+/*!
+ * @brief simplyfied popen() function, function takes a command and executes the given command in an child process. Commands are liunx/unix shell commands.
+ * @param command argument is a pointer to a null-terminated string containing a shell command line. This command is passed to /bin/sh using  the -c  flag;  interpretation, if any, is performed by the shell.
+ * @param type a pointer to a null-terminated string  which  must  contain either the letter 'r' for reading or the letter 'w' for writing.
+ * @return  FILE Pointer connecting parent and shell in success case, NULL in case it fails
+ */
 extern FILE *mypopen (const char *command, const char *type);
 
+/*!
+ * @brief The mypclose() function waits for the associated process to terminate and returns the exit status of the command as returned by wait(2).
+ * @param stream a filestream created by a previous mypopen command,
+ * @return the exit status of the terminated child or if the exit status failed, -1
+ */
 extern int mypclose (FILE *stream);
 
 #endif //MYPOPEN_MPOPEN_H
